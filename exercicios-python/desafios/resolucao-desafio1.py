3bc98c2125b3a6818b4ba2403b6f32905a9ab0ec
Andre Silva <andremarciliosilva@gmail.com>
2026-01-13 15:34:15 -0300

    Refatoração do desafio1

--- exercicios-python/desafios/resolucao-desafio1.py
+++ exercicios-python/desafios/resolucao-desafio1.py
@@ -21,11 +21,10 @@ print ("Bem-vindo ao jogo, escolha Pedra, Papel ou Tesoura, as regras são as se
 "-Papel vence a Pedra (cobrindo-a).\n" 
 "Se ambos os jogadores escolherem o mesmo objeto, a rodada resulta em um empate.\n") 
 
-# Solicita as entradas e verifica se a opção é válida
+# Solicita as entradas e verifica se a opção é válida,
+# se não for solicita a entrada novamente.
 
 while True:
-
-    # Solicita entradas dos jogadores e converte pra minúsculo pra verificar as condições
 
     jogador1 = input("Jogador 1, digite uma opção: ")
     opcao_jogador1 = jogador1.lower()
@@ -35,30 +34,21 @@ while True:
     opcoes = ["pedra", "papel", "tesoura"]
 
     if opcao_jogador1 in opcoes and opcao_jogador2 in opcoes:
-        pass
+        break
     else:
         print("Opção inválida, digite novamente!")
 
 # Verifica se há um vencedor ou empate
 
-    if opcao_jogador1 == "pedra" and opcao_jogador2 == "tesoura":
-        print("O Jogador 1 venceu!")
 
-    elif opcao_jogador2 == "pedra" and opcao_jogador1 == "tesoura":
-        print ( "O Jogador 2 venceu!")
+if opcao_jogador1 == opcao_jogador2:
+    print ("Empatou, joguem novamente!")
 
-    elif opcao_jogador1 == "tesoura" and opcao_jogador2 == "papel":
-        print("O Jogador 1 venceu!")
+elif  opcao_jogador1 == "pedra" and opcao_jogador2 == "tesoura" \
+    or opcao_jogador1 == "tesoura" and opcao_jogador2 == "papel" \
+    or  opcao_jogador1 == "papel" and opcao_jogador2 == "pedra":
+    print("O Jogador 1 venceu!")
 
-    elif opcao_jogador2 == "tesoura" and opcao_jogador1 == "papel":
-        print ( "O Jogador 2 venceu!")
-
-    elif opcao_jogador1 == "papel" and opcao_jogador2 == "pedra":
-        print("O Jogador 1 venceu!")
-
-    elif opcao_jogador2 == "papel" and opcao_jogador1 == "pedra":
-        print ( "O Jogador 2 venceu!")
-
-    elif opcao_jogador1 == opcao_jogador2:
-        print ("Empatou, joguem novamente!")
+else:
+    print ("O jogador 2 venceu!")
 
